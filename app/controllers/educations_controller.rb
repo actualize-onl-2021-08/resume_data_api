ea13c04794dad9740eba355482b3287d1de8a8a5
Dean Itani <muhiedin.itani@gmail.com>
2021-12-06 21:10:31 -0600

    create action complete

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -2,6 +2,22 @@ class EducationsController < ApplicationController
   def index
     educations = Education.all
     render json: educations.as_json
-
+  end
+  
+  def create
+    education = Education.new(
+      start_date: params[:start_date],
+      end_date: params[:end_date],
+      degree: params[:degree],
+      university_name: params[:university_name],
+      details: params[:details],
+      student_id: params[:student_id]
+    )
+    if education.save
+      render json: { message: "Education created successfully" }, status: :created
+    else
+      render json: { errors: education.errors.full_messages }, status: :bad_request
+    end
+    # render json: {message: "hello"}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get "/experiences" => "experiences#index"
   post "/experiences" => "experiences#create"
   get "/educations" => "educations#index"
+  post "/educations" => "educations#create"
 end
 
