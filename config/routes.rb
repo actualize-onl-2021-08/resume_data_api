71688837be3edcdbd8391b1da1bf14f26dfed5f1
d.mathisen <drew.a.mathisen@gmail.com>
2021-12-07 17:10:06 -0700

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,12 @@ Rails.application.routes.draw do
   get "/skills/:id" => "skills#show" 
   patch "/skills/:id" => "skills#update"
   delete "skills/:id" => "skills#destroy"
+  #capstonse routes
+  get "/capstones" => "capstones#index"
+  post "/capstones" => "capstones#create"
+  get "/capstones/:id" => "capstones#show" 
+  patch "/capstones/:id" => "capstones#update"
+  delete "capstones/:id" => "capstones#destroy"
+
 end
 
